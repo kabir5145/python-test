da1f16e26a421bbe2b97dc303cc8521955d73158
Kabir5145 <kabir10112006@gmail.com>
2026-02-28 01:23:28 +0530

    solved 8 questions of NumPy basics

--- main_3.py
+++ main_3.py
@@ -31,3 +31,53 @@ import numpy as np
 # arr = np.arange(1,11)
 # arr[arr % 2 != 0] = -1
 # print(arr)
+
+# Exercise 6:
+# Convert a 1D array to a boolean array where all positive values become True.
+# array = np.array([-1,0,1,2])
+# bool_array = array > 0
+# print(bool_array)
+
+# Exercise 7:
+# Replace all even numbers in a 1D array with their negative.
+# array = np.arange(1,11)
+# array[array % 2 == 0 ] = -array[array % 2 == 0]
+# print(array)
+
+# Exercise 8:
+# Create a random 3x3 matrix and normalize it.
+# matrix = np.random.random((3,3))
+# normalized_matrix = (matrix - np.mean(matrix)) / np.std(matrix)
+# print(normalized_matrix)
+
+# Exercise 9:
+# Calculate the sum of the diagonal elements of a 3x3 matrix.
+# matrix = np.random.random((3,3))
+# print(matrix)
+# sum_of_diagonal = np.trace(matrix)
+# print(sum_of_diagonal)
+
+# Exercise 10:
+# Find the indices of non-zero elements from [1,2,0,0,4,0].
+# array = np.array([1,2,0,0,4,0])
+# non_zero_indices = np.nonzero(array)
+# print(non_zero_indices)
+
+# Exercise 11:
+# Reverse a 1D array (first element becomes the last).
+# array = np.arange(1,11)
+# reverse_array = np.flip(array)
+# print(reverse_array)
+
+# Exercise 12:
+# Create a 3x3 identity matrix.
+# identity_matrix = np.eye(3)
+# Or
+# identity_matrix = np.identity(3)
+# print(identity_matrix)
+
+# Exercise 13:
+# Reshape a 1D array to a 2D array with 5 rows and 2 columns.
+# array = np.arange(10)
+# arr = array.reshape(5,2)
+# print(arr)
